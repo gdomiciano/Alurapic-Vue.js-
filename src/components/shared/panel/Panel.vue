4469df2fd0e816159acc5ac4aedbbb6bc8f5cf6d
Geisy Domiciano <geisydomiciano@gmail.com>
2017-08-05 04:56:42 -0300

    add panel component

---
+++ src/components/shared/panel/Panel.vue
@@ -0,0 +1,43 @@
+
+
+<template>
+    <div class="panel">
+        <h2 class="panel-title">{{ title }}</h2>
+        <div class="panel-content">
+        </div>
+    </div>
+</template> 
+
+<script>
+    export default {
+        name: 'panel',
+        props: ['title'],
+    }
+</script>
+
+<style lang="scss">
+
+    .panel {
+        border-radius: 10px 10px 0 0 ;
+        box-shadow: #666 0 0 10px;
+        display: inline-block;
+        text-align: center;
+        vertical-align: top;
+        width: 100%;
+
+        .panel-title {
+            border-bottom: #4A78A6 2px inset;
+            background: linear-gradient(0deg, #799dc2, transparent);
+            margin: 0;
+            line-height: 30px;
+            font-size: 20px;
+        }
+        
+        .panel-pic {
+            width:100%;
+        }
+    }
+
+
+    
+</style>
