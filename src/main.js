579f72e79cfeac88fb698b78525af71dd7453972
Geisy Domiciano <geisydomiciano@gmail.com>
2017-08-06 01:59:27 -0300

    add vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueResource from 'vue-resource';
+import VUeRouter from 'vue-router';
 
 Vue.use(VueResource);
+Vue.use(VUeRouter);
 
 new Vue({
   el: '#app',
