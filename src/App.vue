fda60d1a0a162de4862013848d563dca775e8c08
Geisy Domiciano <geisydomiciano@gmail.com>
2017-08-06 02:32:43 -0300

    add SPA functionality and dinamic menu

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
 
 <template>
     <div class="wrapper">
+        <nav>
+            <ul>
+                <li v-for="route in routes">
+                    <router-link :to="route.path ? route.path : '/'">{{ route.title }}</router-link>
+                </li>
+            </ul>            
+        </nav>
         <router-view></router-view>
     </div>
 </template> 
 
 <script>
-    
+    import { routes } from './routes';
+    export default {
+        data (){
+            return {
+                routes,
+            }
+        }
+    }
 </script>
 
 <style scoped lang="scss">
