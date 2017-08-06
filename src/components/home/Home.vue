b6cdf6dd24902e8c8b23d0d016b2e9875034737c
Geisy Domiciano <geisydomiciano@gmail.com>
2017-08-06 04:00:06 -0300

    add action button component without functionality

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -5,7 +5,8 @@
         <ul class="photos-list">
             <li class="photo-item" v-for="photo of photosWithFilter">
                 <panel :title="photo.title">
-                    <responsive-img :url="photo.url" :titulo="photo.title"/>
+                    <responsive-img :url="photo.url" :titulo="photo.title" />
+                    <action-button type="button" label="Remove" @click.native="removeItem(photo)"/>
                 </panel>
            </li>
         </ul>
@@ -15,11 +16,13 @@
 <script>
     import Panel from '../shared/panel/Panel.vue';
     import ResponsiveImg from '../shared/responsive-img/ResponsiveImg.vue';
+    import Button from '../shared/button/Button.vue'
 
     export default {
         components: {
             'panel': Panel, 
             'responsive-img':ResponsiveImg,
+            'action-button': Button,
         },
         data() {
             return {
@@ -43,6 +46,11 @@
                 .then(res => res.json())
                 .then(photos => this.photos = photos, error => console.log(error));
         },
+        methods: {
+            removeItem(photo) {
+                alert('remove the picture'+photo.title);
+            }
+        }
     }
 </script>
 
